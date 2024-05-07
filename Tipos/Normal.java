243d1e308e3f0474eb210419842fa2f1d252ce9d
Pedro Henrique Pinheiro Linhares <p175807@dac.unicamp.br>
2024-05-07 17:15:29 -0300

    organizando em pastas

---
+++ Tipos/Normal.java
@@ -0,0 +1,11 @@
+
+public class Normal extends Tipo {
+    
+    public Normal(String img, String descricao, String resposta){
+        super(img, descricao, resposta);
+    }
+
+    public String getDescricao(){
+        return super.
+    }
+}
