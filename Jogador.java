5e7de9085191585fd4310a14db29efad7f0368c6
Giancarlo Bonvenuto <giancarlo.bonvenuto@gmail.com>
2024-05-17 12:25:47 -0300

    Comecei a mexer no Jogador, mas preciso da classe Pokemon

--- Jogador.java
+++ Jogador.java
@@ -1,5 +1,33 @@
-class Jogador{
+import java.util.Scanner;
+
+class Jogador {
   private String nome;
-  private Array<Pokemon> pokemons;
-  private Array<Item> itens;
+  private Pokemon[] pokemons = new Pokemon[4]; // Cada jogador terá somente 4 pokemons
+  private List<Item> itens = new ArrayList<Item>();
+
+  Jogador(String nome) {
+    this.nome = nome;
+  }
+
+  public void SelecionarPokemons(){
+    boolean pokemon_valido = false;
+    String pokemon_escolhido;
+    for(int i = 0; i<pokemons.length; i++){
+      pokemon_valido = false;
+      Scanner scanner = new Scanner(System.in);
+      System.out.println(Pokemon.lista);
+      do{
+        System.out.println("Digite o nome do pokemon que você deseja escolher");
+        pokemon_escolhido = scanner.next();
+        if (Class.forName(pokemon_escolhido) != null) {
+          pokemon_valido = true;
+        }
+        else{
+          System.out.println("Não foi possível encontrar este pokemon, tente outro");
+        }
+      }while(pokemon_valido);
+      
+      pokemons[i] = Class.forName(pokemon_escolhido);
+  }
+
 }
