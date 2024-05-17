b6f933818d81d9cfa20df1443dfaae204f0e5857
Pedro Henrique Pinheiro Linhares <p175807@dac.unicamp.br>
2024-05-17 12:24:29 -0300

    tipos

---
+++ Tipos/Gelo.java
@@ -0,0 +1,23 @@
+package Tipos;
+
+import utils.Ataque;
+import utils.Tipo;
+
+public class Gelo extends Tipo {
+    
+    public Gelo(String img){
+        super(img);
+        super.setTipo("Gelo");
+    }
+
+    public int defesa(Tipo golpe){
+        if(buscaFraqueza(golpe)){
+            return 2*golpe.getDano();
+        } else if (buscaImunidade(golpe)){
+            return 0;
+        } else if (buscaResistencia(golpe)){
+            return (1/2) * golpe.getDano();
+        }
+        return golpe.getDano();
+    }
+}
--- Tipos/Normal.java
+++ Tipos/Normal.java
@@ -1,11 +1,22 @@
+package Tipos;
+import utils.Tipo;
+import utils.Ataque;
 
-public class Normal extends Tipo {
-    
-    public Normal(String img, String descricao, String resposta){
-        super(img, descricao, resposta);
+public class Normal extends Tipo {    
+
+    public Normal(String img){
+        super(img);
+        super.setTipo("Normal");
     }
 
-    public String getDescricao(){
-        return super.
+    public int defesa(Tipo golpe){
+        if(buscaFraqueza(golpe)){
+            return 2*golpe.getDano();
+        } else if (buscaImunidade(golpe)){
+            return 0;
+        } else if (buscaResistencia(golpe)){
+            return (1/2) * golpe.getDano();
+        }
+        return golpe.getDano();
     }
 }
