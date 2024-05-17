cc82c07597565d9c8ac82f5f16fbc23c39c2e9ba
Pedro Henrique Dos Reis Arcoverde <ra254719@wolverine.lab.ic.unicamp.br>
2024-05-17 12:29:21 -0300

    implementação pokemon na branch errada

---
+++ Pokemon.java
@@ -0,0 +1,113 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Pokemon {
+
+
+    // iniciando a classe pokemon
+
+    // variáveis 
+    String nome;
+    int vida = 100; // todos os pokemons iniciam com 100 de vida
+
+    // vraiáveis espciais
+    Tipo tipo;  // todo pokemon possui um tipo espcífico --> apenas um por pokemon
+
+    Ataque ataque;// todo pokemon pode atacar
+
+    ArrayList<Ataque> ataques = new ArrayList<>();
+
+    Defesa defesa; // todo pokemon pode se defender
+
+    T debuff;
+
+    ArrayList <T> debuffs = new ArrayList<>(); 
+
+    public Pokemon(String nome, Tipo tipo){
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+
+
+    // métodos implementação
+
+    public int getVida(){
+        return this.vida; 
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public Tipo getTipo(){
+        return this.tipo;
+    }
+
+
+
+
+
+    // método de atacar
+    public int ataca(Pokemon pokemon){ 
+        // deve escolher o ataque --> é bom que isso seja uma subescolha de um menu de ações
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escolha o ataque"); // suponho que exista uma função getNomeAtaque
+
+        int i = 0;
+        int dano;
+        for (Ataque at: ataques){
+            i++;
+            System.out.println(i + at.getNomeAtaque);
+        }
+        
+        int escolha = sc.nextInt();
+
+        do {
+            System.out.println("Escolha um número de 1 a 4:");
+            while (!sc.hasNextInt()) {
+                System.out.println("Entrada inválida. Por favor, insira um número."); // resolve se o usuário inserir nada
+                sc.next(); // Limpar entrada inválida
+            }
+
+            escolha = sc.nextInt();
+
+            dano = switch (escolha) {
+                case 1 -> ataques.get(0).getDano();  // suponho a existência de um método getDano na classe Ataque
+                    
+                case 2 -> ataques.get(1).getDano();
+            
+                case 3 -> ataques.get(2).getDano();
+                    
+                case 4 -> ataques.get(3).getDano();
+                    
+                default -> System.out.println("Escolha inválida. Por favor, insira um número entre 1 e 4.");
+            };
+
+        } while (escolha < 1 || escolha > 4); // restringe a escolha do usuário
+
+        sc.close();
+
+    }
+
+    public void defender(Pokemon pokemon, int dano){ // na função de lógica de jogo eu vou ter que lembrar disso ( associar o ataque de um pokemon ao outro)
+        
+        // a função de defesa tem que considerar o tipo do atcante e do meu pokemon
+        Tipo tipoAdversário = pokemon.getTipo;
+
+        if (tipoAdversário == this.tipo.getResistence()){ // suponho que tenha uma classe para pegar a resistencia e fraqueza de cada tipo na classe tipo
+            this.vida -= dano/2;
+        }
+
+        else if(tipoAdversário == this.tipo.getFraqueza()){
+            this.vida -= dano*2;
+        }
+
+        debuff = pokemon.ataque.getDebuff();  // suponho que o debuff esteja na classe de ataque
+        if (debuff != null){
+            debuffs.add(debuff);
+        }
+    }
+
+
+
+}
