fbd35e704975d39a921644ba2d28bec4960f65d4
ThiagoATNascimento <135238223+ThiagoATNascimento@users.noreply.github.com>
2024-05-19 11:09:07 -0300

    commit de teste (só pra ver se vai dar certo)

--- ataques/Acido.java
+++ ataques/Acido.java
@@ -3,9 +3,11 @@ package ataques;
 import utils.*;
 import tipos.*;
 
-public class Acido extends Ataque{
-    public Acido(){
-        super("Ácido", 50, Veneno);
-    }
+public class Acido extends Ataque {
+
+  static String nome = "Ácido";
+  static int dano = 50;
+  static Class<? extends Tipo> tipo = Veneno.class;
+
 }
-//veneno
+// veneno
