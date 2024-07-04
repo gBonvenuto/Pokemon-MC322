f7ed2afd006f620f3dcbdb43da1f610672120228
Pedro Henrique Pinheiro Linhares <p175807@dac.unicamp.br>
2024-07-04 15:35:15 -0300

    comecando batalha interface

---
+++ src/main/java/utils/Batalha_Interface.java
@@ -0,0 +1,17 @@
+package utils;
+
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicScrollBarUI;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Batalha_Interface extends JFrame {
+    private Icon pokemon1;
+    private Icon pokemon2;
+
+    public Batalha
+}
