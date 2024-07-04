bf23d8d9b7b3cfd2640f1302dccf23e3755b9ee9
Giancarlo Bonvenuto <giancarlo.bonvenuto@gmail.com>
2024-07-04 13:29:09 -0300

    Começando a deixar a interface inicial mais coerente

--- src/main/java/utils/InicializarJogador_Interface.java
+++ src/main/java/utils/InicializarJogador_Interface.java
@@ -1,6 +1,8 @@
 package utils;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicScrollBarUI;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,12 +14,22 @@ public class InicializarJogador_Interface extends JFrame {
   private JTextField nomeField;
   private JButton selecionarButton;
   private JTextArea outputArea;
+  private JList listaPokemons;
   private List<Pokemon> pokemonsEscolhidos;
 
-  public Jogador jogador;
+  private int jogador = 0;
 
   public InicializarJogador_Interface() {
-    setTitle("Seleção de Jogador");
+    if(Jogo.getJogador1() == null)
+      jogador = 1;
+    else if(Jogo.getJogador2() == null)
+      jogador = 2;
+
+    if(jogador == 1)
+      setTitle("Seleção do Jogador 1");
+    else if(jogador == 2)
+      setTitle("Seleção do Jogador 2");
+
     setSize(400, 300);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setLayout(new BorderLayout());
@@ -25,25 +37,35 @@ public class InicializarJogador_Interface extends JFrame {
     // Painel para o nome do jogador
     JPanel nomePanel = new JPanel();
     nomePanel.setLayout(new FlowLayout());
-    nomePanel.add(new JLabel("Nome do jogador:"));
+    if(jogador==1)
+      nomePanel.add(new JLabel("Nome do jogador 1:"));
+    if(jogador==2)
+      nomePanel.add(new JLabel("Nome do jogador 2:"));
     nomeField = new JTextField(20);
     nomePanel.add(nomeField);
+
+    // Pokemons selecionados
+    JPanel pokemonsSelecionadosPanel = new JPanel();
+    pokemonsSelecionadosPanel.setLayout(new BorderLayout());
+    pokemonsSelecionadosPanel.add(new JLabel("Pokemon 1:"));
+    pokemonsSelecionadosPanel.add(new JLabel("\nPokemon 2:"));
+    pokemonsSelecionadosPanel.add(new JLabel("\nPokemon 3:"));
+    pokemonsSelecionadosPanel.add(new JLabel("\nPokemon 4:"));
 
     // Painel para selecionar pokémons
     JPanel pokemonPanel = new JPanel();
     pokemonPanel.setLayout(new BorderLayout());
     pokemonPanel.setBorder(BorderFactory.createTitledBorder("Seleção de Pokémons"));
 
-    outputArea = new JTextArea(10, 30);
-    outputArea.setEditable(false);
-    JScrollPane scrollPane = new JScrollPane(outputArea);
-    pokemonPanel.add(scrollPane, BorderLayout.CENTER);
+    listaPokemons = new JList<String>(Pokemon.pokemon.lista());
+    pokemonPanel.add(listaPokemons, BorderLayout.CENTER);
 
     selecionarButton = new JButton("Selecionar Pokémons");
     pokemonPanel.add(selecionarButton, BorderLayout.SOUTH);
 
     // Adicionando componentes à janela principal
     add(nomePanel, BorderLayout.NORTH);
+    add(pokemonsSelecionadosPanel, BorderLayout.EAST);
     add(pokemonPanel, BorderLayout.CENTER);
 
     // Inicialização dos pokémons escolhidos
@@ -86,12 +108,16 @@ public class InicializarJogador_Interface extends JFrame {
 
           pokemonsEscolhidos.add(pokemonEscolhido);
           outputArea.append("Pokemon escolhido: " + pokemonEscolhido.getNome() + "\n");
+
         }
 
         outputArea.append("\nJogador: " + nomeJogador + "\n");
       }
     });
-    Jogo.setJogador1(new Jogador(nomeField.toString(), pokemonsEscolhidos));
+    if(Jogo.getJogador1() == null)
+      Jogo.setJogador1(new Jogador(nomeField.toString(), pokemonsEscolhidos));
+    else if(Jogo.getJogador2() == null)
+      Jogo.setJogador2(new Jogador(nomeField.toString(), pokemonsEscolhidos));
   }
 
   public static void main(String[] args) {
