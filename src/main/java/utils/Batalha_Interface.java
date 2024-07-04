f90ddd425fe07632244d3ebdc71ae20931116f87
Giancarlo Bonvenuto <giancarlo.bonvenuto@gmail.com>
2024-07-04 17:15:41 -0300

    Imagem resized

--- src/main/java/utils/Batalha_Interface.java
+++ src/main/java/utils/Batalha_Interface.java
@@ -44,8 +44,8 @@ public class Batalha_Interface extends JFrame {
     panel.add(new JLabel("Vida: "+pokemonAtual.getVida()+"/300"), c);
     try{
       BufferedImage bulbassauro = ImageIO.read(new File("src/main/java/resources/bulbassaur.png"));
-      JLabel image_label = new JLabel(new ImageIcon(bulbassauro));
-      image_label.setSize(new Dimension(1,1));
+      JLabel image_label = new JLabel(new ImageIcon(getScaledImage(bulbassauro, 100, 100)));
+      image_label.setPreferredSize(new Dimension(100,100));
       panel.add(image_label, c);
     }
     catch(IOException e){
@@ -54,6 +54,17 @@ public class Batalha_Interface extends JFrame {
 
     return panel;
   }
+
+  private Image getScaledImage(Image srcImg, int w, int h){
+    BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+    Graphics2D g2 = resizedImg.createGraphics();
+
+    g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+    g2.drawImage(srcImg, 0, 0, w, h, null);
+    g2.dispose();
+
+    return resizedImg;
+}
 
   public static void main(String[] args) {
     SwingUtilities.invokeLater(new Runnable() {
